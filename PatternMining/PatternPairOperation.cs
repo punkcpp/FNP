71c33974901766d14ddd598aeeb3c71343782d53
punkcpp <punkcpp@gmail.com>
2014-07-25 15:21:21 -0700

    SubgraphIsomorphism & PatternPairOperation

---
+++ PatternMining/PatternPairOperation.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PatternMining
+{
+    class PatternPairOperation
+    {
+        public Graph Pattern1;
+        public Graph Pattern2;
+        public List<Graph> new_patterns;
+        public int R;
+
+        public PatternPairOperation(Graph Pattern1, Graph Pattern2, int R)
+        {
+            this.Pattern1 = Pattern1;
+            this.Pattern2 = Pattern2;
+            new_patterns = new List<Graph>();
+            this.R = R;
+        }
+
+        public bool qualified()
+        {
+            if (Math.Abs(Pattern1.n - Pattern2.n) > 1)
+                return false;
+
+            Dictionary<string, int> map1 = new Dictionary<string, int>();
+            Dictionary<string, int> map2 = new Dictionary<string, int>();
+            for (int u = 0; u < Pattern1.n; ++u)
+            {
+                string label = Pattern1.getLabel(u);
+                if (!map1.ContainsKey(label))
+                    map1[label] = 1;
+                else
+                    map1[label] = map1[label] + 1;
+            }
+
+            for (int u = 0; u < Pattern2.n; ++u)
+            {
+                string label = Pattern2.getLabel(u);
+                if (!map2.ContainsKey(label))
+                    map2[label] = 1;
+                else
+                    map2[label] = map2[label] + 1;
+            }
+
+            int dif_cnt = 0;
+            foreach (string label in map1.Keys)
+            {
+                int cnt1 = map1[label];
+                int cnt2 = 0;
+                if (map2.ContainsKey(label))
+                    cnt2 = map2[label];
+                dif_cnt += Math.Abs(cnt1 - cnt2);
+            }
+
+            if (dif_cnt > 1)
+                return false;
+            else
+                return true;
+        }
+
+        public int subPatternType(Graph sp)
+        {
+            bool flag = false;
+            for (int u = 0; u < sp.n; ++u)
+            {
+                if (sp.getDeg(u) == 0 && u != sp.getPivot())
+                {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag)
+                return 1;   //sp has no isolated node or the isolated node is the pivot
+            else
+                return 0;   //sp has one isolated node which is not the pivot
+        }
+
+        public List<Graph> generateNewPatterns(List<Graph> FreqPatterns)
+        {
+            new_patterns.Clear();
+            if (!qualified())
+                return new_patterns;
+
+            for (int u = 0; u < Pattern2.n; ++u)
+            {
+                for (int i = 0; i < Pattern2.adj[u].Count; ++i)
+                {
+                    int v = Pattern2.adj[u][i];
+                    if (u <= v)
+                    {
+                        Graph subPattern = Pattern2.removeEdge(u, v);
+                        int type = subPatternType(subPattern);
+                        if (type == 0)
+                        {
+                            //todo
+                        }
+                        else if (type == 1)
+                        {
+                            //todo
+                        }
+                    }
+                }
+            }
+
+            return new_patterns;
+        }
+
+    }
+}
---
+++ PatternMining/SubgraphIsomorphism.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PatternMining
+{
+    class SubgraphIsomorphism
+    {
+        public Graph g1;
+        public Graph g2;
+        public int R;
+        public List<int>[] Nodes1;
+        public List<int>[] Nodes2;
+        public int[] Map;
+        public bool Isomorphic;
+        public bool[] Used;
+
+        public SubgraphIsomorphism(Graph g1, Graph g2, int R) 
+        {
+            this.g1 = g1;
+            this.g2 = g2;
+            this.R = R;
+            Nodes1 = new List<int>[R];
+            Nodes2 = new List<int>[R];
+            for (int i = 0; i < R; ++i)
+            {
+                Nodes1[i] = new List<int>();
+                Nodes2[i] = new List<int>();
+            }
+            Map = new int[g2.n];
+            Isomorphic = false;
+            Used = new bool[g1.n];
+        }
+
+        public bool checkEdges()
+        {
+            for (int u = 0; u < g2.n; ++u)
+            {
+                int u1 = Map[u];
+                for (int v = u + 1; v < g2.n; ++v)
+                {
+                    int v1 = Map[v];
+                    if (!(g2.adj[u].Contains(v) && g1.adj[u1].Contains(v1)))
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        public List<List<int>> findPotential()
+        {
+            List<List<int>> potential = new List<List<int>>();
+            for (int u = 0; u < g2.n; ++u)
+            {
+                potential.Add(new List<int>());
+            }
+
+            for (int i = 0; i < R; ++i)
+            {
+                for (int i1 = 0; i1 < Nodes1[i].Count; ++i1)
+                {
+                    int u = Nodes1[i][i1];
+                    for (int i2 = 0; i2 < Nodes2[i].Count; ++i2)
+                    {
+                        int v = Nodes2[i][i2];
+                        if (g2.getLabel(v).Equals(g1.getLabel(u)))
+                            potential[v].Add(u);
+                    }
+                }
+            }
+            return potential;
+        }
+
+        public void dfs(List<List<int>> potential, int index)
+        {
+            if (index >= potential.Count)
+            {
+                if (checkEdges())
+                    Isomorphic = true;
+                return;
+            }
+            for (int i = 0; i < potential[index].Count; ++i)
+            {
+                int u = potential[index][i];
+                if (!Used[u])
+                {
+                    Map[index] = u;
+                    Used[u] = true;
+                    dfs(potential, index + 1);
+                    if (Isomorphic)
+                        return;
+                    Used[u] = false;
+                }
+            }
+
+        }
+
+        public bool containPattern()
+        {
+            Isomorphic = false;
+            if (g1.n < g2.n) return false;
+
+            int pivot_g = g1.getPivot();
+            int pivot_p = g2.getPivot();
+
+            bool[] vis = new bool[g1.n];
+            for (int i = 0; i < vis.Length; ++i)
+            {
+                vis[i] = false;
+            }
+            int[] que = new int[g1.n];
+            int front =0, rear = 0;           
+            que[rear++] = pivot_g;
+            vis[pivot_g] = true;
+
+            int step = 0;
+            for (int i = 0; i < Nodes1.Length; ++i)
+            {
+                Nodes1[i].Clear();
+            }
+            while (front < rear)
+            {
+                int tmp_rear = rear;
+                while (front < tmp_rear)
+                {
+                    int u = que[front++];
+                    Nodes1[step].Add(u);
+
+                    for (int i = 0; i < g1.adj[u].Count; ++i)
+                    {
+                        int v = g1.adj[u][i];
+                        if (vis[v] == false)
+                        {
+                            vis[v] = true;
+                            que[rear++] = v;
+                        }
+                    }
+                }
+                step++;
+                if (step >= R)
+                    break;
+            }
+
+            for (int i = 0; i < vis.Length; ++i)
+            {
+                vis[i] = false;
+            }
+            front = rear = 0;           
+            que[rear++] = pivot_p;
+            vis[pivot_p] = true;
+
+            step = 0;
+            for (int i = 0; i < Nodes2.Length; ++i)
+            {
+                Nodes2[i].Clear();
+            }
+            while (front < rear)
+            {
+                int tmp_rear = rear;
+                while (front < tmp_rear)
+                {
+                    int u = que[front++];
+                    Nodes2[step].Add(u);
+
+                    for (int i = 0; i < g2.adj[u].Count; ++i)
+                    {
+                        int v = g2.adj[u][i];
+                        if (vis[v] == false)
+                        {
+                            vis[v] = true;
+                            que[rear++] = v;
+                        }
+                    }
+                }
+                step++;
+                if (step >= R)
+                    break;
+            }
+
+            List<List<int>> potential = findPotential();
+
+            for (int i = 0; i < potential.Count; ++i)
+            {
+                if (potential[i].Count == 0)
+                    return false;
+            }
+            for (int i = 0; i < g1.n; ++i)
+            {
+                Used[i] = false;
+            }
+            dfs(potential, 0);
+            return Isomorphic;
+        }
+    }
+}
